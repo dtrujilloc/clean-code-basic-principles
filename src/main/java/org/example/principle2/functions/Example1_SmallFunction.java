6a58519911728f79a7322c6f69a1f41f1f09f6b1
dtrujilloc <daniel.trujilloc@pragma.com.co>
2023-06-12 19:26:09 -0500

    se agrega clase de primer ejemplo de funciones

---
+++ src/main/java/org/example/principle2/functions/Example1_SmallFunction.java
@@ -0,0 +1,10 @@
+package org.example.principle2.functions;
+
+/**
+ * Las clases deben ser lo mas pequeñas posibles, tanto, como para que solo tengan una responsabilidad o proposito.
+ */
+public class Example1_SmallFunction {
+
+    
+
+}
