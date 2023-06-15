b3f3cd11492bfcc21e8d94a908e7b43e85cba520
dtrujilloc <daniel.trujilloc@pragma.com.co>
2023-06-15 18:40:53 -0500

    se agrega paquete para principio 3 - comentarios. y se realizan ejemplos de malos comentarios

---
+++ src/main/java/org/example/principle3/comments/badcomments/Example2_CommentedCode.java
@@ -0,0 +1,22 @@
+package org.example.principle3.comments.badcomments;
+
+/**
+ * Tener secciones o bloques de codigos comentados por que creemos que en algun momento lo vamos a necesitar.
+ * Esto no se debe hacer, ya que primeramente vamos a ensuciar mucho el codigo, y si necesitaramos ese cosigo mas adelante,
+ * lo podriamos recuperar gracias a la herramienta de control de versiones que estemos utilizando.
+ */
+public class Example2_CommentedCode {
+
+    /**
+     * Un ejemplo puede ser cuando refactorizamos alguna funcion y comentamos la vieja por si algo pasa. Por ejemplo, tenemos
+     * una funcion que crea un usuario la cual la refactorizamos y dejamos la anterior comentada.
+     */
+    public void createUserNew(String name, String lastName) {
+
+    }
+
+//    public void createUserOld(String name, String lastName, String address) {
+//
+//    }
+
+}
