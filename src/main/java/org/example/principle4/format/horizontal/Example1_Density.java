0038cb7ba4bee66d3e46dcc90de1b80882384f60
dtrujilloc <daniel.trujilloc@pragma.com.co>
2023-06-20 17:34:14 -0500

    se crea principio 4 - formato

---
+++ src/main/java/org/example/principle4/format/horizontal/Example1_Density.java
@@ -0,0 +1,7 @@
+package org.example.principle4.format.horizontal;
+
+/**
+ * La densidad horizontal del codigo
+ */
+public class Example1_Density {
+}
---
+++ src/main/java/org/example/principle4/format/horizontal/Example2_Indentation.java
@@ -0,0 +1,5 @@
+package org.example.principle4.format.horizontal;
+
+public class Example2_Indentation {
+
+}
