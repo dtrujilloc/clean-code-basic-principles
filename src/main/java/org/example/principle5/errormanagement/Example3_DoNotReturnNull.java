28b636b620a5b054d7c263c92b07c8c7888f8e77
dtrujilloc <daniel.trujilloc@pragma.com.co>
2023-06-21 21:20:14 -0500

    se crea clase para ejempo de no devolver null

---
+++ src/main/java/org/example/principle5/errormanagement/Example3_DoNotReturnNull.java
@@ -0,0 +1,9 @@
+package org.example.principle5.errormanagement;
+
+/**
+ * No se debe retornar o devolver null en los metodos, ya que esto hace obliga a los programadores a hacer validaciones de
+ * null en muchos puntos del codigo, cosa que no deberia suceder a menos que sea ese sea un respuesta esperada. Pero no
+ * es conveniente devolver null.
+ */
+public class Example3_DoNotReturnNull {
+}
